df1f41c403c96040809ed0881f95a562342c2632
Jonathan Daggerhart <jonathan@daggerhart.com>
2016-09-19 10:20:41 -0400

    better button targeting

--- easyupload.js
+++ easyupload.js
@@ -2,7 +2,7 @@
     Drupal.behaviors.autoUpload = {
         attach: function (context, settings) {
             $('form', context).on('change', 'input.form-file', function() {
-                $(this).next('.form-submit').mousedown();
+                $(this).closest('.form-item').find('.form-submit').mousedown();
             });
         }
     };
